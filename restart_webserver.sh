0812935f62d1893ec2e0f002e9ef05b2a4827358
Giovanni Ganzinotti <Giovanni.Ganzinotti@klm.com>
2020-01-09 10:35:09 +0100

    improved restart script

--- restart_webserver.sh
+++ restart_webserver.sh
@@ -2,9 +2,20 @@
 
 sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
+pkill sudoku
+pkill gunicorn
+
 sudo systemctl daemon-reload
 sudo systemctl start sudoku
 sudo systemctl enable sudoku
+
 sudo nginx -t
 sudo systemctl restart nginx
+
+echo "Waiting 3 seconds for gunicorn to restart"
+sleep 3
+sudo systemctl status sudoku
+
+echo "Waiting 3 seconds for gunicorn and load logs"
+sleep 3
 sudo journalctl -u sudoku
